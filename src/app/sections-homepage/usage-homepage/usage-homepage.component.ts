3fd34b2ccee5a6b2a4720c119838252901607d37
VeronicaVassallo <vvassallo94@gmail.com>
2024-07-09 22:28:39 +0200

    echarts added

--- src/app/sections-homepage/usage-homepage/usage-homepage.component.ts
+++ src/app/sections-homepage/usage-homepage/usage-homepage.component.ts
@@ -3,6 +3,9 @@ import { UsageService } from '../../services/usage.service';
 import { ActivatedRoute } from '@angular/router';
 import { Usage } from '../../models/usage.model';
 
+//echarts
+import * as echarts from 'echarts';
+
 @Component({
   selector: 'app-usage-homepage',
   templateUrl: './usage-homepage.component.html',
@@ -31,5 +34,35 @@ export class UsageHomepageComponent implements OnInit {
         },
       });
     }
+
+    //echarts
+
+    type EChartsOption = echarts.EChartsOption;
+
+    var chartDom = document.getElementById('main')!;
+    var myChart = echarts.init(chartDom);
+    var option: EChartsOption;
+
+    option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar',
+          showBackground: true,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)',
+          },
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
   }
 }
