3d3f194493c9bf2af3b32c23bbdb0b461f4ded2f
VeronicaVassallo <vvassallo94@gmail.com>
2024-07-11 19:12:52 +0200

    Bootstrap 5 added + modal for payment

--- src/app/components/table-payments/table-payments.component.ts
+++ src/app/components/table-payments/table-payments.component.ts
@@ -1,18 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Payment } from '../../models/payment.model';
 
 @Component({
   selector: 'app-table-payments',
   templateUrl: './table-payments.component.html',
-  styleUrl: './table-payments.component.css',
+  styleUrls: ['./table-payments.component.css'],
 })
-export class TablePaymentsComponent implements OnInit {
-  isOpen: boolean = false;
+export class TablePaymentsComponent {
   @Input() paymentsList: Payment[] = [];
-  openModal() {
-    this.isOpen = !this.isOpen;
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
 }
